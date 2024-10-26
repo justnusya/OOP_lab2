18105731ce8a40300654644c4928cfb936d6e2d5
justnusya <sahaidak.yana1123@vu.cdu.edu.ua>
2024-10-26 11:25:34 +0300

    Added a draft of 3 constructors for copying from another instance, from double array, from jagged array

--- OOP lab2/MatrixData.cs
+++ OOP lab2/MatrixData.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace OOP_lab2
 {
-    class MatrixData
+    public partial class MyMatrix
     {
+        private double[,] matrixElements;
+        public MyMatrix(MyMatrix copy)
+        {
+            matrixElements = copy.matrixElements;
+        }
+        public MyMatrix(double[,]elements)
+        {
+            matrixElements = elements;
+        }
+        public MyMatrix(double[][]elements)
+        {
+            bool isRectangle = true;
+            for (int i = 1; i < elements.Length; i++)
+            {
+                if (elements[i].Length != elements[0].Length)
+                {
+                    isRectangle = false;
+                    throw new ArgumentException("Матриця повинна бути прямокутною");
+                }
+            }
+
+            if (isRectangle==true)
+            {
+                for(int i =0; i < elements.Length; i++)
+                {
+                    for(int j=0; j < elements[0].Length; j++)
+                    {
+                        matrixElements[i, j] = elements[i][j];
+                    }
+                }
+            }
+        }
     }
 }
