681895b07d797fb583086eb22c354a871b58ca0a
justnusya <sahaidak.yana1123@vu.cdu.edu.ua>
2024-10-26 10:46:16 +0300

    Add project files.

---
+++ OOP lab2/Main_project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_lab2
+{
+    class Main_project
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
